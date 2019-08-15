bcd7c2dc607181b91e205673b4a8e962cc28ee56
Aleksander Spro <abspro@gmail.com>
2019-08-15 19:48:10 +0200

    Cleanup

--- ImportImages.ps1
+++ ImportImages.ps1
@@ -6,11 +6,3 @@ $SourceThumb='/Source:C:\temp\teabagthumbs2'
 # $DestinationEmu='/Dest:http://127.0.0.1:10000/devstoreaccount1/images'
 # $DestinationEmuKey='Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=='
 # & $AzCopy $Source $DestinationEmu /S /BlobType:block /DestType:"Blob" /Y /DestKey:$DestinationEmuKey /SetContentType:image/jpeg
-
-$Destination='/Dest:https://collectionteststorage.blob.core.windows.net/images/'
-$DestinationKey='XjMZ94aWTJ7LcpmhdC8c0Dc8WQahrJofVaLksxlKgsfJBhKh+KksjX1ULYO+sn81jtOSQQ6k9SJnf7wZfQfaxw=='
-& $AzCopy $Source $Destination /S /BlobType:block /DestType:"Blob" /Y /DestKey:$DestinationKey /SetContentType:image/jpeg
-
-$DestinationThumb='/Dest:https://collectionteststorage.blob.core.windows.net/thumbnails/'
-$DestinationThumbKey='XjMZ94aWTJ7LcpmhdC8c0Dc8WQahrJofVaLksxlKgsfJBhKh+KksjX1ULYO+sn81jtOSQQ6k9SJnf7wZfQfaxw=='
-& $AzCopy $SourceThumb $DestinationThumb /S /BlobType:block /DestType:"Blob" /Y /DestKey:$DestinationThumbKey /SetContentType:image/jpeg
