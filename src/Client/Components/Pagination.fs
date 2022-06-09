3e2e97df52862ff71bd05dee1b4d768f933eb8ea
Aleksander Spro <abspro@gmail.com>
2022-06-09 16:58:28 +0200

    Hide pagebar when we only have 1 page

--- src/Client/Components/Pagination.fs
+++ src/Client/Components/Pagination.fs
@@ -28,24 +28,22 @@ let totalPages (model: Model) =
 // https://stackoverflow.com/questions/39670902/pagination-calculation-algorithm
 // https://stackoverflow.com/questions/46382109/limit-the-number-of-visible-pages-in-pagination
 
-
-let getPageList (model: Model) =
-  let totalPages = totalPages model
+let getPageList numberOfPages (model: Model) =
   let sideWidth:int = if MaxButtons < 9 then 1 else 2;
   let leftWidth = (MaxButtons - sideWidth * 2 - 3) >>> 1;
   let rightWidth = (MaxButtons - sideWidth * 2 - 2) >>> 1;
   //printf "%i %i %i %i" totalPages sideWidth leftWidth rightWidth
-  if totalPages <= MaxButtons then
-    seq { 1 .. totalPages }
+  if numberOfPages <= MaxButtons then
+    seq { 1 .. numberOfPages }
   else if (model.currentPage <= MaxButtons - sideWidth - 1 - rightWidth) then
-    printf "case 1 %i %i" (totalPages - sideWidth + 1) (MaxButtons - sideWidth - totalPages)
-    seq { yield! seq {1 .. MaxButtons - sideWidth - 1}; yield! [0]; yield! seq {totalPages - sideWidth + 1 .. totalPages} }
-  else if (model.currentPage >= totalPages - sideWidth - 1 - rightWidth) then
+    printf "case 1 %i %i" (numberOfPages - sideWidth + 1) (MaxButtons - sideWidth - numberOfPages)
+    seq { yield! seq {1 .. MaxButtons - sideWidth - 1}; yield! [0]; yield! seq {numberOfPages - sideWidth + 1 .. numberOfPages} }
+  else if (model.currentPage >= numberOfPages - sideWidth - 1 - rightWidth) then
     printf "case 2"
-    seq { yield! seq {1 .. sideWidth}; yield! [0]; yield! seq {totalPages - sideWidth - 1 - rightWidth - leftWidth .. totalPages} }
+    seq { yield! seq {1 .. sideWidth}; yield! [0]; yield! seq {numberOfPages - sideWidth - 1 - rightWidth - leftWidth .. numberOfPages} }
   else 
     printf "case 3"
-    seq { yield! seq {1 .. sideWidth}; yield! [0]; yield! seq {model.currentPage - leftWidth .. model.currentPage + rightWidth}; yield! [0]; yield! seq {totalPages - sideWidth + 1 .. totalPages} }
+    seq { yield! seq {1 .. sideWidth}; yield! [0]; yield! seq {model.currentPage - leftWidth .. model.currentPage + rightWidth}; yield! [0]; yield! seq {numberOfPages - sideWidth + 1 .. numberOfPages} }
 
 let createPageButtons (currentPage: int) (dispatch: ExternalMsg -> unit) pageList=
   pageList
@@ -57,14 +55,19 @@ let createPageButtons (currentPage: int) (dispatch: ExternalMsg -> unit) pageLis
   |> Seq.toList
 
 let view (model: Model) (dispatch: ExternalMsg -> unit) =
-  Pagination.pagination [ Pagination.IsCentered; Pagination.CustomClass "is-sticky-top" ]
-    [
-      Pagination.previous [ Props [ Disabled (model.currentPage < 2); OnClick (fun ev -> dispatch SearchPagePrevious) ] ]
-        [ str "Previous" ]
-      Pagination.next [ Props [Disabled (model.currentPage = (totalPages model)); OnClick (fun ev -> dispatch SearchPageNext)] ]
-        [ str "Next page" ]
-      Pagination.list [ ]
-        [
-          model|> getPageList |> createPageButtons model.currentPage dispatch |> ofList
-        ]
-    ]
+  let numberOfPages = totalPages model
+  match numberOfPages with
+  | 0 -> nothing
+  | 1 -> nothing
+  | _ ->
+    Pagination.pagination [ Pagination.IsCentered; Pagination.CustomClass "is-sticky-top" ]
+      [
+        Pagination.previous [ Props [ Disabled (model.currentPage < 2); OnClick (fun ev -> dispatch SearchPagePrevious) ] ]
+          [ str "Previous" ]
+        Pagination.next [ Props [Disabled (model.currentPage = numberOfPages); OnClick (fun ev -> dispatch SearchPageNext)] ]
+          [ str "Next page" ]
+        Pagination.list [ ]
+          [
+            model|> getPageList numberOfPages |> createPageButtons model.currentPage dispatch |> ofList
+          ]
+      ]
