6c18ccc402f43d64a599de30e2bd54d61e7edc49
Aleksander Spro <abspro@gmail.com>
2022-06-16 17:14:27 +0200

    Refactor navbar to feliz

--- src/Client/Components/Navbar/View.fs
+++ src/Client/Components/Navbar/View.fs
@@ -2,20 +2,12 @@ module Client.Components.Navbar.View
 
 open Elmish.Navigation
 open Fable.Core.JsInterop
-open Fable.React
-open Fable.React.Props
-open Fable.Import
-open Fulma
+open Feliz
+open Feliz.Bulma
 
 open Client.Components.Navbar.Types
 open Client.Navigation
 
-type HTMLAttr =
-     | [<CompiledName("data-target")>] DataTarget of string
-     | [<CompiledName("aria-label")>] AriaLabel of string
-     | [<CompiledName("aria-expanded")>] AriaExpanded of bool
-     | [<CompiledName("aria-hidden")>] AriaHidden of bool
-     interface IHTMLProp
 
 let goToUrl (e: Browser.Types.MouseEvent) =
     e.preventDefault()
@@ -28,54 +20,69 @@ let goToUrlAndToggleBurger page dispatch (e: Browser.Types.MouseEvent) =
   dispatch ToggleBurger |> ignore
   goToUrl e |> ignore
 
-let viewLink description page currentPage dispatch =
-  Navbar.Item.a [ Navbar.Item.IsActive (page = currentPage)
-                  Navbar.Item.Modifiers [Modifier.TextSize (Screen.Mobile, TextSize.Is3)]
-                  Navbar.Item.Props [ Style [ Padding "0 20px" ]
-                                      Href (Client.Navigation.toPath page)
-                                      OnClick (goToUrlAndToggleBurger page dispatch) ] ]
-                  [ str description]
-
-let brand (model: Model) dispatch =
-  Navbar.Brand.div [] [
-      Navbar.Item.a [ Navbar.Item.Props [ Href "\\" ]] [
-        img [ Src "/svg/teapot.svg"; Style [Width 30; Height 30;]; Alt ""]
-      ]
-      a [
-          Role "button"
-          Class "navbar-burger"
-          AriaLabel "menu"
-          AriaExpanded model.isBurgerOpen
-          DataTarget "navMenu"
-          OnClick (fun _ -> dispatch ToggleBurger)
-        ] [
-          span [AriaHidden true] []
-          span [AriaHidden true] []
-          span [AriaHidden true] []
-      ]
+  
+let viewLink (description: string) page currentPage dispatch =
+  Bulma.navbarItem.a [
+    if (page = currentPage) then Bulma.navbarItem.isActive
+    size.isSize4Mobile
+    prop.style [ style.padding (0, 20) ]
+    prop.href (Client.Navigation.toPath page)
+    prop.onClick (goToUrlAndToggleBurger page dispatch)
+    prop.text description
   ]
 
-// https://github.com/Fulma/Fulma/issues/46
-// https://github.com/MangelMaxime/fulma-demo/blob/master/src/App.fs#L16-L65
+let brand (model: Model) dispatch =
+  Bulma.navbarBrand.div [
+    Bulma.navbarItem.a [
+      prop.href "\\"
+      prop.children [
+        Html.img [
+          prop.src "/svg/teapot.svg"
+          prop.alt ""
+          prop.style [
+            style.width 30
+            style.height 30
+          ]
+        ]
+      ]
+    ]
+    Html.a [
+      prop.role "button"
+      prop.className "navbar-burger"
+      prop.ariaLabel "menu"
+      prop.ariaExpanded model.isBurgerOpen
+      prop.custom("data-target", "navMenu")
+      prop.onClick (fun _ -> dispatch ToggleBurger)
+      prop.children [
+        Html.span [ prop.ariaHidden true ]
+        Html.span [ prop.ariaHidden true ]
+        Html.span [ prop.ariaHidden true ]
+      ]
+    ]
+  ]
+
 let navMenu currentPage canAdd (model: Model) dispatch =
-  Navbar.menu [
-     Navbar.Menu.IsActive model.isBurgerOpen
-    ] [
-      Navbar.End.div [] [
+  Bulma.navbarMenu [
+    if model.isBurgerOpen then Bulma.navbarMenu.isActive
+    prop.children [
+      Bulma.navbarEnd.div [
         yield viewLink "Dashboard" Page.Dashboard currentPage dispatch
         yield viewLink "Teabags" Page.Teabags currentPage dispatch
-        if canAdd then yield viewLink "New" (Page.TeabagNew "") currentPage dispatch
+        if canAdd then yield viewLink "New" (Page.Teabag 0) currentPage dispatch
         yield viewLink "Logout" Page.Logout currentPage dispatch
       ]
     ]
+  ]
 
 let view currentPage canAdd (model: Model) dispatch =
-    Navbar.navbar [ Navbar.Props [Role "navigation"
-                                  AriaLabel "main naviation"]
-                    Navbar.CustomClass "is-fixed-top"
-        ] [
-          Container.container [] [
-            brand model dispatch
-            navMenu currentPage canAdd model dispatch
-          ]
-        ]
+  Bulma.navbar [
+    prop.role "navigation"
+    prop.ariaLabel "main naviation"
+    prop.className "is-fixed-top"
+    prop.children [
+      Bulma.container [
+        brand model dispatch
+        navMenu currentPage canAdd model dispatch
+      ]
+    ]
+  ]
