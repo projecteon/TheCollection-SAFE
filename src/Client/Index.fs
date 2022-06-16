e56bd8f1a9a77bc1d5880a3f03fa0a078362f772
Aleksander Spro <abspro@gmail.com>
2022-06-16 17:32:40 +0200

    Refactor index.fs to feliz

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,18 +1,11 @@
 module Index
 
-open Elmish
-open Elmish.Navigation
-open Elmish.React
 open Fable.Core.JsInterop
-open Fable.React
-open Fable.React.Props
-open Fulma
+open Feliz
+open Feliz.Bulma
 open Elmish.HMR // needs to be last
 
 open ClientTypes
-open State
-open Client.Navigation
-
 
 // look into https://chriscourses.com/blog/loading-fonts-webpack
 importAll "./index.scss"
@@ -32,10 +25,10 @@ let viewPage model dispatch =
 let view model dispatch =
   match model.PageModel with
   | PageModel.LoginPageModel m ->
-    div [ Class "loginPage" ] (viewPage model dispatch)
+    Html.div [ prop.className "loginPage"; prop.children (viewPage model dispatch) ] 
   | _ ->
-    Container.container [ ] [
+    Bulma.container [
       Client.Components.Navbar.View.view model.CurrentPage (Client.Extensions.canEdit model.User) model.Navbar (NavbarMsg >> dispatch)
-      hr [Style [MarginTop 0]]
-      div [ ] (viewPage model dispatch)
+      Html.hr [ prop.style [ style.marginTop 0 ] ]
+      Html.div (viewPage model dispatch)
     ]
