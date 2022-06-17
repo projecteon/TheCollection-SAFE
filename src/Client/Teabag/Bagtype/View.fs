f966df6ed0e1529a31d8f175644baef2e5578a60
Aleksander Spro <abspro@gmail.com>
2022-06-17 14:00:33 +0200

    Refactor to Feliz from fulma

--- src/Client/Teabag/Bagtype/View.fs
+++ src/Client/Teabag/Bagtype/View.fs
@@ -1,47 +1,63 @@
 module Client.Teabag.Bagtype.View
 
 open Fable.React
-open Fable.React.Props
 open Fable.FontAwesome
-open Fulma
+open Feliz
+open Feliz.Bulma
 
 open Client.Teabag.Bagtype.Types
 open Server.Api.Dtos
-open HtmlProps
 open Client.ReactHelpers
-open Client.Teabag.Bagtype
 
 let viewForm (bagtype: Bagtype) (model:Model) dispatch  =
-  printf "%b %b %b %b" (not (State.isValid model.validationErrors))  model.isWorking  (model.data = model.originaldata) (State.areEqual model.originaldata model.data)
-  form [AutoComplete Off; Disabled model.isWorking] [
-    Field.div [ ] [
-      Label.label [ Label.Option.For "name" ] [
-        str "Name"
-      ]
-      Control.div [ ] [
-        Input.text [
-          yield Input.Placeholder "Ex: name";
-          yield Input.ValueOrDefault bagtype.name.String;
-          yield Input.Option.Id "name";
-          yield Input.OnChange (getValue >> NameChanged >> dispatch)
-          if (not (model.validationErrors |> State.getNameErrors |> Seq.isEmpty)) then
-            yield Input.Color IsDanger
-          else if model.doValidation then
-            yield Input.Color IsSuccess ]
-      ]
-      Client.FulmaHelpers.inputError (model.validationErrors |> State.getNameErrors |> List.ofSeq) |> ofList
-    ]
-    Field.div [ Field.IsGrouped; Field.IsGroupedCentered ] [
-      Control.div [ ] [
-        Button.button [ Button.Color IsPrimary; Button.IsFullWidth; Button.Disabled (model.data.IsSome && model.data.Value.id.Int = 0); Button.OnClick (fun ev -> ev.preventDefault(); ev.stopPropagation(); dispatch New) ] [
-          if model.isWorking then yield Fa.i [ Fa.Solid.CircleNotch; Fa.Spin ] [ ]
-          else yield str "New"
+  Html.form [
+    prop.autoComplete "off"
+    prop.disabled model.isWorking
+    prop.children [
+      Bulma.field.div [
+        Bulma.label [ prop.for' "name"; prop.text "Name" ]
+        Bulma.control.div [
+          Bulma.input.text [
+            prop.placeholder "Ex: name"
+            prop.valueOrDefault bagtype.name.String
+            prop.id "name"
+            prop.onChange (getValue >> NameChanged >> dispatch)
+            if (not (model.validationErrors |> State.getNameErrors |> Seq.isEmpty)) then
+              color.isDanger
+            else if model.doValidation then
+              color.isSuccess
+          ]
         ]
+        Client.FulmaHelpers.inputError (model.validationErrors |> State.getNameErrors |> List.ofSeq) |> ofList
       ]
-      Control.div [ ] [
-        Button.button [ Button.Color IsPrimary; Button.IsFullWidth; Button.Disabled ((not (State.isValid model.validationErrors)) || model.isWorking || model.data = model.originaldata); Button.OnClick (fun ev -> ev.preventDefault(); ev.stopPropagation(); dispatch ValidateAndSave) ] [
-          if model.isWorking then yield Fa.i [ Fa.Solid.CircleNotch; Fa.Spin ] [ ]
-          else yield str "Save"
+      Bulma.field.div [
+        Bulma.field.isGrouped
+        Bulma.field.isGroupedCentered
+        prop.children [
+          Bulma.control.div [ 
+            Bulma.button.button [
+              color.isPrimary
+              button.isFullWidth
+              prop.disabled (model.data.IsSome && model.data.Value.id.Int = 0)
+              prop.onClick (fun ev -> ev.preventDefault(); ev.stopPropagation(); dispatch New)
+              prop.children [
+                if model.isWorking then Fa.i [ Fa.Solid.CircleNotch; Fa.Spin ] [ ]
+                else Html.text "New"
+              ]
+            ]
+          ]
+          Bulma.control.div [ 
+            Bulma.button.button [
+              color.isPrimary
+              button.isFullWidth
+              prop.disabled ((not (State.isValid model.validationErrors)) || model.isWorking || model.data = model.originaldata)
+              prop.onClick (fun ev -> ev.preventDefault(); ev.stopPropagation(); dispatch ValidateAndSave)
+              prop.children [
+                if model.isWorking then Fa.i [ Fa.Solid.CircleNotch; Fa.Spin ] [ ]
+                else Html.text "Save"
+              ]
+            ]
+          ]
         ]
       ]
     ]
@@ -52,7 +68,7 @@ let view (model:Model) (dispatch: Msg -> unit) =
   | Some x ->
     viewForm x model dispatch
   | None ->
-    div [ClassName "pageloader is-white is-active"; Style [Position PositionOptions.Absolute]] []
+    Html.div [ prop.className "pageloader is-white is-active"; prop.style [ style.position.absolute ] ]
 
 let headerText (model: Model) =
   if model.data.IsSome && model.data.Value.id.Int > 0 then
